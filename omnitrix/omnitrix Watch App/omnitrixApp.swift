1b03e906d6b1352855cc1de9611535e9240778fd
Sweet-Poision <ur.dev6@gmail.com>
2026-05-23 19:26:18 +0530

    initial dev commit

---
+++ omnitrix/omnitrix Watch App/omnitrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  omnitrixApp.swift
+//  omnitrix Watch App
+//
+//  Created by Utsav Raj on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct omnitrix_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
